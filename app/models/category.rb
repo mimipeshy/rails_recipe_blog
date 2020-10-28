7e3dea1b3bcebaf8b6d6f7dabadea85fee976fba
mimi peshy <mimipeshy@gmail.com>
2020-10-28 23:39:08 +0300

    add associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :articles, through: :recipe_categories, dependent: :delete_all
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :articles
+  has_many :votes
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
