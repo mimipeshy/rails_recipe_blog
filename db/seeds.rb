1df74ab316b3e19998980860e60641aeb3446868
mimi peshy <mimipeshy@gmail.com>
2020-10-30 01:59:23 +0300

    add seed to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(id: 1, name: 'cakes', priority: 1)
+Category.create(id: 2, name: 'pastries', priority: 2)
+Category.create(id: 3, name: 'drinks', priority: 3)
+Category.create(id: 4, name: 'food', priority: 4)
