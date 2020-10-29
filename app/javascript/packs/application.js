534e5a7bf4d76edaa04eece881da826f7592d79f
mimi peshy <mimipeshy@gmail.com>
2020-10-29 20:21:13 +0300

    add bootstrap files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+import "bootstrap"
+import "../stylesheets/application"
 
 require("@rails/ujs").start()
 require("turbolinks").start()
